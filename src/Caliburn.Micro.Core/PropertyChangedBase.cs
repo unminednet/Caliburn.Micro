3e555196644f98e43211b12b29f4af1142b9cd8b
megazyz <megasys@intro.hu>
2025-04-21 19:13:26 +0200

    PropertyChangedBase inherits from ReactiveObject

--- src/Caliburn.Micro.Core/PropertyChangedBase.cs
+++ src/Caliburn.Micro.Core/PropertyChangedBase.cs
@@ -3,40 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
+using ReactiveUI;
 
 namespace Caliburn.Micro
 {
-    /// <summary>
-    /// A base class that implements the infrastructure for property change notification and automatically performs UI thread marshalling.
-    /// </summary>
-    [DataContract]
-    public class PropertyChangedBase : INotifyPropertyChangedEx
+    public class PropertyChangedBase : ReactiveObject, INotifyPropertyChangedEx
     {
-        /// <summary>
-        /// Creates an instance of <see cref = "PropertyChangedBase" />.
-        /// </summary>
-        public PropertyChangedBase()
+        public virtual bool IsNotifying
         {
-            IsNotifying = true;
-        }
-
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public virtual event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Enables/Disables property change notification.
-        /// Virtualized in order to help with document oriented view models.
-        /// </summary>
-        public virtual bool IsNotifying { get; set; }
-
-        /// <summary>
-        /// Raises a change notification indicating that all bindings should be refreshed.
-        /// </summary>
-        public virtual void Refresh()
-        {
-            NotifyOfPropertyChange(string.Empty);
+            get { return AreChangeNotificationsEnabled(); }
+            set { throw new NotSupportedException(); }
         }
 
         /// <summary>
@@ -45,66 +21,27 @@ namespace Caliburn.Micro
         /// <param name = "propertyName">Name of the property.</param>
         public virtual void NotifyOfPropertyChange([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
         {
-            if (IsNotifying && PropertyChanged != null)
+
+            if (IsNotifying)
             {
                 if (PlatformProvider.Current.PropertyChangeNotificationsOnUIThread)
                 {
-                    OnUIThread(() => OnPropertyChanged(new PropertyChangedEventArgs(propertyName)));
+                    Execute.OnUIThread(() => this.RaisePropertyChanged(propertyName));
                 }
                 else
                 {
-                    OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+                    this.RaisePropertyChanged(propertyName);
                 }
             }
+
         }
 
-        /// <summary>
-        /// Notifies subscribers of the property change.
-        /// </summary>
-        /// <typeparam name = "TProperty">The type of the property.</typeparam>
-        /// <param name = "property">The property expression.</param>
-        public void NotifyOfPropertyChange<TProperty>(Expression<Func<TProperty>> property)
+
+        public virtual void Refresh()
         {
-            NotifyOfPropertyChange(property.GetMemberInfo().Name);
-        }
-
-        /// <summary>
-        /// Raises the <see cref="PropertyChanged" /> event directly.
-        /// </summary>
-        /// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        protected void OnPropertyChanged(PropertyChangedEventArgs e)
-        {
-            PropertyChanged?.Invoke(this, e);
-        }
-
-        /// <summary>
-        /// Executes the given action on the UI thread
-        /// </summary>
-        /// <remarks>An extension point for subclasses to customise how property change notifications are handled.</remarks>
-        /// <param name="action"></param>
-        protected virtual void OnUIThread(System.Action action) => action.OnUIThread();
-
-        /// <summary>
-        /// Sets a backing field value and if it's changed raise a notification.
-        /// </summary>
-        /// <typeparam name="T">The type of the value being set.</typeparam>
-        /// <param name="oldValue">A reference to the field to update.</param>
-        /// <param name="newValue">The new value.</param>
-        /// <param name="propertyName">The name of the property for change notifications.</param>
-        /// <returns></returns>
-        public virtual bool Set<T>(ref T oldValue, T newValue, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(oldValue, newValue))
-            {
-                return false;
-            }
-
-            oldValue = newValue;
-
-            NotifyOfPropertyChange(propertyName ?? string.Empty);
-
-            return true;
+            NotifyOfPropertyChange(string.Empty);
         }
     }
+
+
 }
