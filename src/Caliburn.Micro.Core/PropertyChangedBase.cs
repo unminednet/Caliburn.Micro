743dbeae1a7d39006c07d46ed7b02079f63776ed
megazyz <megasys@intro.hu>
2025-10-31 23:33:57 +0100

    avalonia version bump

--- src/Caliburn.Micro.Core/PropertyChangedBase.cs
+++ src/Caliburn.Micro.Core/PropertyChangedBase.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using System.Reactive;
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading;
 using ReactiveUI;
 
 namespace Caliburn.Micro
 {
+
+
+    /// <summary>
+    ///     PropertyChangedBase implementation inherited from ReactiveUI.ReactiveObject
+    ///
+    ///     BREAKING CHANGE: UI thread synchronization does not work except for Refresh() and NotifyOfPropertyChange()
+    ///
+    ///     Another solution would be to implement IReactiveObject in the original
+    ///     PropertyChangedBase class, but that does not work well with AOT
+    /// </summary>
+    /// <seealso cref="ReactiveUI.ReactiveObject" />
+    /// <seealso cref="ReactiveUI.IReactiveObject" />
+    /// <seealso cref="Caliburn.Micro.INotifyPropertyChangedEx" />
     public class PropertyChangedBase : ReactiveObject, INotifyPropertyChangedEx
     {
         public virtual bool IsNotifying
         {
             get { return AreChangeNotificationsEnabled(); }
-            set { throw new NotSupportedException(); }
+            set { throw new NotSupportedException("Use DelayChangeNotifications()"); }
         }
 
         /// <summary>
@@ -42,6 +60,5 @@ namespace Caliburn.Micro
             NotifyOfPropertyChange(string.Empty);
         }
     }
-
 
 }
